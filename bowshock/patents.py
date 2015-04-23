67f6a24fcb6f9b0bb96ee6ef1e7ef76a88be61a2
Emir Ozer <emirozer@yandex.com>
2015-04-23 17:55:13 +0200

    added patents endpoint and tests

---
+++ bowshock/patents.py
@@ -0,0 +1,59 @@
+# The NASA patent portfolio is available to benefit US citizens. 
+# Through partnerships and licensing agreements with industry, 
+# these patents ensure that NASAs investments in pioneering research find secondary uses that benefit the economy
+# create jobs, and improve quality of life. This endpoint provides structured, searchable developer access to NASAs patents that have been curated to support technology transfer.
+
+import requests
+from helpers import nasa_api_key, bowshock_logger
+
+logger = bowshock_logger()
+
+def patents(query=None, concept_tags=None, limit=None):
+    '''
+    HTTP REQUEST
+
+    GET https://api.data.gov/nasa/patents
+
+    QUERY PARAMETERS
+
+    Parameter	Type	Default	Description
+    query	string	None	Search text to filter results
+    concept_tags	bool	False	Return an ordered dictionary of concepts from the patent abstract
+    limit	int	all	number of patents to return
+    api_key	string	DEMO_KEY	api.data.gov key for expanded usage
+    EXAMPLE QUERY
+
+    https://api.data.gov/nasa/patents/content?query=temperature&limit=5&api_key=DEMO_KEY
+
+    '''
+    base_url = "http://api.data.gov/nasa/patents/content?"
+
+
+    if not query:
+        raise ValueError("search query is missing, which is mandatory.")
+    elif not isinstance(query, str):
+        try:
+            query = str(query)
+        except:
+            raise ValueError("query has to be type of string")
+    else:
+        base_url += "query="+ query + "&"
+
+    if concept_tags == True:
+        base_url += "concept_tags=True" + "&"
+
+    if limit:
+        if not isinstance(limit, int):
+            logger.error("The limit arg you provided is not the type of int, ignoring it")
+
+        base_url += "limit=" + str(limit) + "&"
+
+    req_url = base_url + "api_key=" + nasa_api_key()
+    
+    logger.warning("patents endpoint, dispatching request : %s ", req_url)
+
+    response = requests.get(req_url)
+    
+    logger.warning("Retrieved response from patents endpoint: %s", response.text)
+
+    return response
---
+++ tests/patents_tests.py
@@ -0,0 +1,41 @@
+import unittest
+import requests
+import sys
+from time import sleep
+
+sys.path.append("../bowshock/")
+
+from patents import patents
+
+
+class patents_UnitTests(unittest.TestCase):
+        
+    def test_patents_endpoint_full(self):
+        
+        r = patents(query="temperature", concept_tags=True, limit=5)
+        self.assertEqual(r.status_code, 200)
+        sleep(2)
+    
+    def test_patents_endpoint_notags(self):
+        
+        r = patents(query="temperature", limit=5)
+        self.assertEqual(r.status_code, 200)
+        sleep(2)
+
+    def test_patents_endpoint_nolimit(self):
+        
+        r = patents(query="temperature")
+        self.assertEqual(r.status_code, 200)
+        sleep(2)
+    
+    
+
+if __name__ == "__main__":
+
+    # Build the test suite
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(patents_UnitTests))
+
+    # Execute the test suite
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(len(result.errors) + len(result.failures))
