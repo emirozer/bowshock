012622554004017b4f8794f0f47fc1e045f2659d
Emir Ozer <emirozer@yandex.com>
2015-04-23 22:46:34 +0200

    added asterank api

--- bowshock/asterank.py
+++ bowshock/asterank.py
@@ -1,1 +1,58 @@
 # http://www.asterank.com/api
+# The Asterank database is a thin layer over the NASA/JPL Small Body Database, merged with JPL delta-v data, published asteroid mass data, 
+# and their own calculations.
+
+#The database currently runs on mongodb and queries must adhere to mongo's json format for a 'find' operation.
+
+
+import requests
+import json
+from helpers import bowshock_logger
+
+
+logger = bowshock_logger()
+
+
+def asterank(query=None, limit=None):
+    '''
+    Format
+    Requests are of the form:
+
+    http://asterank.com/api/asterank?query={query}&limit={limit}
+    Response data formats are largely derived from NASA/JPL's Small Body Database query browser. Exceptions to this are the delta-v field (dv), the mass field (GM), and the normalized spectral type field (spec). Additional Asterank scores are included: closeness, price ($), and overall score.
+
+    Sample Request
+    This request returns an asteroid with a roughly circular orbit, low inclination, and semi-major axis less than 1.5 AU:
+
+    /api/asterank?query={"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}}&limit=1
+
+    '''
+    
+    base_url = "http://asterank.com/api/asterank?"
+    
+    if query:
+        try:
+            query = json.dumps(query)
+            
+            base_url += "query=" + query + "&"
+        except:
+            raise ValueError("query= param is not valid json.")
+    else:
+        raise ValueError("query= param is missing, expecting json data format.")
+
+    if limit:
+        if not isinstance(limit, int):
+            logger.error("The limit arg you provided is not the type of int, ignoring it")
+        base_url += "limit=" + str(limit)
+    else:
+        raise ValueError("limit= param is missing, expecting int")
+    
+    logger.warning("Asterank API, dispatching request : %s ", base_url)
+
+    response = requests.get(base_url)
+
+    
+    logger.warning("Retrieved response from Asterank API: %s", response.text)
+
+    return response
+
---
+++ tests/asterank_tests.py
@@ -0,0 +1,28 @@
+import unittest
+import requests
+import sys
+from time import sleep
+
+sys.path.append("../bowshock/")
+
+from asterank import asterank
+
+
+class asterank_UnitTests(unittest.TestCase):
+        
+    def test_asterank_api_full(self):
+        
+        r = asterank(query={"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}},limit=1)
+        self.assertEqual(r.status_code, 200)
+        sleep(2)
+
+
+if __name__ == "__main__":
+
+    # Build the test suite
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(asterank_UnitTests))
+
+    # Execute the test suite
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(len(result.errors) + len(result.failures))
